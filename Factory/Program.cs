4c23a635a1130a768c1ba9b7a52d8be805e1c4cc
Charles <deepblue_zw@hotmail.com>
2023-01-01 12:46:08 +0800

    add comments

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,4 +1,11 @@
 ﻿namespace Factory;
+
+/// <summary>
+/// 工厂模式
+/// </summary>
+/// <remarks>
+/// 工厂方法模式通过面向对象编程中的多态性来将对象的创建延迟到具体工厂中，从而解决了简单工厂模式中存在的问题，也很好地符合了开放封闭原则（即对扩展开发，对修改封闭）。
+/// </remarks>
 class Program
 {
     static void Main(string[] args)
