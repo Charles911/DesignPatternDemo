2fd4a27b18165f3638a22dd3dcf2c9fde1bd92df
Charles <deepblue_zw@hotmail.com>
2023-01-02 19:38:28 +0800

    Bridge pattern issue fixed

--- Bridge/RemoteControl.cs
+++ Bridge/RemoteControl.cs
@@ -5,20 +5,20 @@ namespace Bridge;
 /// </summary>
 public class RemoteControl
 {
-    public TV Implementor {get; set;}
+    public TV? Implementor { get; set; }
 
     /// <summary>
     /// 开电视机，这里抽象类中不再提供实现了，而是调用实现类中的实现
     /// </summary>
-    public virtual void On() => Implementor.On();
+    public virtual void On() => Implementor!.On();
 
     /// <summary>
     /// 关电视机
     /// </summary>
-    public virtual void Off() => Implementor.Off();
+    public virtual void Off() => Implementor!.Off();
 
     /// <summary>
     /// 换频道
     /// </summary>
-    public virtual void SetChannel() => Implementor.TuneChannel();
+    public virtual void SetChannel() => Implementor!.TuneChannel();
 }
