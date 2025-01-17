bb7ed332513707404482ad5d605ba8e0c21893dc
charles911 <deepblue_zw@hotmail.com>
2025-01-18 07:56:13 +0800

    代码顶层化

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,35 +1,8 @@
-﻿namespace Observer;
+﻿using Observer;
 
-/// <summary>
-/// 观察者模式
-/// </summary>
-/// <remarks>
-/// 观察者模式定义了一种一对多的依赖关系，让多个观察者对象同时监听某一个主题对象，这个主题对象在状态发生变化时，会通知所有观察者对象，使它们能够自动更新自己的行为。
-/// 
-/// 优点：
-/// 1.观察者模式实现了表示层和数据逻辑层的分离，并定义了稳定的更新消息传递机制，并抽象了更新接口，使得可以有各种各样不同的表示层，即观察者。
-/// 2.观察者模式在被观察者和观察者之间建立了一个抽象的耦合，被观察者并不知道任何一个具体的观察者，只是保存着抽象观察者的列表，每个具体观察者都符合一个抽象观察者的接口。
-/// 3.观察者模式支持广播通信。被观察者会向所有的注册过的观察者发出通知。
-/// 
-/// 缺点：
-/// 1.如果一个被观察者有很多直接和间接的观察者时，将所有的观察者都通知到会花费很多时间。
-/// 2.虽然观察者模式可以随时使观察者知道所观察的对象发送了变化，但是观察者模式没有相应的机制使观察者知道所观察的对象是怎样发生变化的。
-/// 3.如果在被观察者之间有循环依赖的话，被观察者会触发它们之间进行循环调用，导致系统崩溃，在使用观察者模式应特别注意这点。
-/// 
-/// 应用场景：
-/// 1.当一个抽象模型有两个方面，其中一个方面依赖于另一个方面，将这两者封装在独立的对象中以使它们可以各自独立地改变和复用的情况下。
-/// 2.当对一个对象的改变需要同时改变其他对象，而又不知道具体有多少对象有待改变的情况下。
-/// 3.当一个对象必须通知其他对象，而又不能假定其他对象是谁的情况下。
-/// </remarks>
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        TenXun tenXun = new TenXunGame("TenXun Game", "Have a new game published ....");
+TenXun tenXun = new TenXunGame("TenXun Game", "Have a new game published ....");
 
-        tenXun.AddObserver(new Subscriber("Learning Hard"));
-        tenXun.AddObserver(new Subscriber("Tom"));
+tenXun.AddObserver(new Subscriber("Learning Hard"));
+tenXun.AddObserver(new Subscriber("Tom"));
 
-        tenXun.Update();
-    }
-}
+tenXun.Update();
